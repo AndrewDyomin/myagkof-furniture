167a7501f5da778a0780c00a5242cd469c86affe
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-08-09 16:58:53 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Hero from "./components/hero";
 import ToCatalogBtns from "./components/toCatalogBtns";
 import Bestsellers from "./components/bestsellers";
 import InfoSection from "./components/infoSection";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <ToCatalogBtns />
       <Bestsellers />
       <InfoSection />
+      <Footer />
     </main>
   );
 }
