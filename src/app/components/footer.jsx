63f1e1e805fd9c5c8fd7bdf10eb6b3aa08014132
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-08-09 18:00:33 +0300

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,5 +1,25 @@
-export default function Footer () {
-    return (
-        <div></div>
-    )
+import clsx from "clsx";
+import Image from "next/image";
+import Logo from "../../../public/icons/myagkof-logo.svg";
+
+export default function Footer() {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <a
+          href="/"
+          className={clsx("flex items-center")}
+        >
+          <Image 
+            src={Logo} 
+            alt="myagkof-logo" 
+            width={40}
+            height={40}
+            />
+          <p className={clsx("ml-2.5 font-bold")}>М&apos;якоff</p>
+        </a>
+      </div>
+    </div>
+  );
 }
